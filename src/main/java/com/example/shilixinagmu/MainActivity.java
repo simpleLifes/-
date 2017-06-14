fce6f08cbb8856239014031e298f2cefe25fcb16
simpleLifes <zmzyf123@163.com>
2017-06-14 17:46:52 +0800

    更新textview
    更新了textview

--- src/main/java/com/example/shilixinagmu/MainActivity.java
+++ src/main/java/com/example/shilixinagmu/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompat {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = new TextView();
+        tv.setText = "开始";
     }
 
     public void setContentView(int contentView) {
